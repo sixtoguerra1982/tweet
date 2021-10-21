cb5872e660912f4c04fd577ccd05cf9729131f29
Sixto Guerra <sixto.guerra1982@gmail.com>
2021-10-20 22:33:17 -0300

    includes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,6 @@
 class PagesController < ApplicationController
   def index
-    @tweets = Tweet.all.order(created_at: :desc).limit(50)
+    # @tweets = Tweet.all.order(created_at: :desc).limit(50)
+    @tweets = Tweet.includes(:user, :likes).order(created_at: :desc).limit(50)
   end
 end
